49d17bd1ced8d738320caefc4eac178bf195b8fe
xeth <adamhung12@gmail.com>
2021-10-15 02:39:43 +0800

    update

--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/xh-dev/xhTg
+module github.com/xh-dev-go/xhTg
 
 go 1.16
 
--- tg.go
+++ tg.go
@@ -3,8 +3,8 @@ package main
 import (
 	"errors"
 	"flag"
-	"github.com/xh-dev/xhTg/getMe"
-	"github.com/xh-dev/xhTg/notification"
+	"github.com/xh-dev-go/xhTg/getMe"
+	"github.com/xh-dev-go/xhTg/notification"
 	"os"
 )
 
